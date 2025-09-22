daca58b622495cbfe50b522c1d341baf6b780f63
JPS <29806111+angeiras-santos@users.noreply.github.com>
2025-09-22 17:51:00 +0100

    Criar classe Conversoes e mover método
    O método ConverterTemperatura foi removido da classe Aritmetica e colocado na classe Conversoes

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -5,21 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Calculos
-{   
+{
     /// <summary>
-     /// Implementa operações aritméticas.
-     /// </summary>
+    /// Implementa operações aritméticas.
+    /// </summary>
     internal static class Aritmetica
     {
-        /// <summary>
-        /// Tipo de conversão de temperatura a executar.
-        /// </summary>
-        public enum ConversaoTemperatura
-        {
-            Nulo = 0,
-            CelsiusFahrenheit,
-            FahrenheitCelsius
-        }
+        
         /// <summary>
         /// Operação soma.
         /// </summary>
@@ -35,19 +27,6 @@ namespace Calculos
         public static int Subtrair(int x, int y)
         {
             return x - y;
-        }
-        public static double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
-        {
-            if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
-            {
-                return (temperatura * 1.8000 + 32);
-            }
-            else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
-            {
-                return ((temperatura - 32) / 1.8000);
-            }
-
-            return -1;
         }
     }
 }
---
+++ Aritmeticas.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos
+{   
+    /// <summary>
+     /// Implementa operações aritméticas.
+     /// </summary>
+    internal static class Aritmetica
+    {
+        /// <summary>
+        /// Tipo de conversão de temperatura a executar.
+        /// </summary>
+        public enum ConversaoTemperatura
+        {
+            Nulo = 0,
+            CelsiusFahrenheit,
+            FahrenheitCelsius
+        }
+        /// <summary>
+        /// Operação soma.
+        /// </summary>
+        /// <returns>Retorna a soma de dois números.</returns>
+        public static int Somar(int x, int y)
+        {
+            return x + y;
+        }
+        /// <summary>
+        /// Operação subtração.
+        /// </summary>
+        /// <returns>Retorna o resultado da subtração de dois números.</returns>
+        public static int Subtrair(int x, int y)
+        {
+            return x - y;
+        }
+        public static double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
+        {
+            if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
+            {
+                return (temperatura * 1.8000 + 32);
+            }
+            else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
+            {
+                return ((temperatura - 32) / 1.8000);
+            }
+
+            return -1;
+        }
+    }
+}
