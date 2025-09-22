c2a21d9f3de655683b2fd0a1397dcb7674c109cc
JPS <29806111+angeiras-santos@users.noreply.github.com>
2025-09-22 17:28:27 +0100

    Criar ficheiro Aritmetica por cópia do ficheiro Program

---
+++ Aritmetica.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos
+{   
+    /// <summary>
+     /// Implementa operações aritméticas.
+     /// </summary>
+    internal class Aritmetica
+    {
+        /// <summary>
+        /// Tipo de conversão de temperatura a executar.
+        /// </summary>
+        public enum ConversaoTemperatura
+        {
+            Nulo = 0,
+            CelsiusFahrenheit,
+            FahrenheitCelsius
+        }
+        /// <summary>
+        /// Operação soma.
+        /// </summary>
+        /// <returns>Retorna a soma de dois números.</returns>
+        public int Somar(int x, int y)
+        {
+            return x + y;
+        }
+        /// <summary>
+        /// Operação subtração.
+        /// </summary>
+        /// <returns>Retorna o resultado da subtração de dois números.</returns>
+        public int Subtrair(int x, int y)
+        {
+            return x - y;
+        }
+        public double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
+        {
+            if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
+            {
+                return (temperatura * 1.8000 + 32);
+            }
+            else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
+            {
+                return ((temperatura - 32) / 1.8000);
+            }
+
+            return -1;
+        }
+    }
+}
